85616a0090e12ff9dd1426d4b0f29d08b8d4f434
Priyanshu20048 <mittal.priyanshu2004@gmail.com>
2025-08-02 23:41:53 +0530

    Loops

---
+++ EASY/1523.java
@@ -0,0 +1,15 @@
+
+https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+class Solution {
+    public int countOdds(int low, int high) {
+        // int count =0;
+        // for (int i =low;i<=high;i++){
+        //     if (i % 2 != 0){
+        //         count++;
+        //     }
+        // }
+        return ((high+1)/2 - low/2);
+        
+    }
+}
